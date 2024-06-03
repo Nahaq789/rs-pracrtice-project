62b65eed569be7a897727fd8920749a0c3fbe92d
Nahaq789 <naha022677@icloud.com>
2024-06-03 13:16:09 +0000

    shift only

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    println!("Hello World!")
+    input! {
+        n: i32, mut a: [i32; n]
+    }
+    let mut flg = true;
+    let mut count = 0;
+    loop {
+        for x in 0..a.len() {
+            if a[x] % 2 != 0 {
+                println!("{}", count);
+                return;
+            }
+        }
+        for b in 0..a.len() {
+            if a[b] % 2 == 0 {
+                a[b] /= 2;
+            } else {
+                println!("{}", count);
+                return;
+            }
+        }
+        count += 1;
+    }
 }
